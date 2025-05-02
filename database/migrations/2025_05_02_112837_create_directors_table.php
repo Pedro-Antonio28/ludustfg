f51e9efc8a5109d5802064960a8b2e06bb7c5437
Alvareitor48 <alvaroelguaperas499@gmail.com>
2025-05-02 13:38:25 +0200

    feat: create migrations v1

---
+++ database/migrations/2025_05_02_112837_create_directors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('directors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('pass');
+            $table->string('profile_img')->nullable();
+            $table->string('school_name');
+            $table->string('school_code');
+            $table->string('school_email');
+            $table->string('school_tel');
+            $table->string('school_type');
+            $table->date('membership_start');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('directors');
+    }
+};
