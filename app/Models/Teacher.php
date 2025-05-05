30b8068472141a8db37b612e85ebf1c9ae616608
AlbertoEsp42 <canaldirectos02@gmail.com>
2025-05-05 12:29:57 +0200

    feat:Creation of teacher Model

---
+++ app/Models/Teacher.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'profile_img',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+
+
+}
