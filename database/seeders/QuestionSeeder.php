31ef50abc5d2186265ac3827dd2e54a4369ee804
Alvareitor48 <alvaroelguaperas499@gmail.com>
2025-05-30 13:34:17 +0200

    refac: remove unused gaps and changed by squares

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -56,7 +56,7 @@ class QuestionSeeder extends Seeder
                 ],
             ],
             [
-                'name' => 'El ___ es el satélite de la Tierra. Y su proclamador se llamaba ___.',
+                'name' => 'El [🔲1] es el satélite de la Tierra. Y su proclamador se llamaba [🔲2].',
                 'type' => 'fill_blank',
                 'mark' => 1.5,
                 'answer' => [
@@ -65,7 +65,7 @@ class QuestionSeeder extends Seeder
                 ],
             ],
             [
-                'name' => 'El ___ procesa los datos. Donde más se guardan cosas es en la ___.',
+                'name' => 'El [🔲1] procesa los datos. Donde más se guardan cosas es en la [🔲2].',
                 'type' => 'fill_multiple',
                 'mark' => 2.0,
                 'answer' => [
